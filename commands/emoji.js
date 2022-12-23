0bae8c67c4645335cd5977b68dc3c8e220fda1a2
jimjam2077 <james.nicholas.thomas@gmail.com>
2022-12-23 10:25:50 +0000

    emoji command list

--- commands/emoji.js
+++ commands/emoji.js
@@ -7,34 +7,36 @@
 
 const { SlashCommandBuilder, SlashCommandSubcommandBuilder } = require('discord.js');
 
-function buildSubCommand(name, desc) {
+function buildSubCommand(name, desc) { //this should go into a helper file
     let sub_cmd = new SlashCommandSubcommandBuilder()
         .setName(name)
         .setDescription(desc)
     return sub_cmd;
 }
 
-let display = buildSubCommand('display', 'displays a custom emoji at a bigger size.')
+let displayemoji = buildSubCommand('display', 'displays a custom emoji at a bigger size');
+let addemoji = buildSubCommand('add', 'adds an emoji using an attachment');
+let delemoji = buildSubCommand('delete', 'deletes an emoji from the emoji list');
+let reactscore = buildSubCommand('score', 'shows the reaction score for a user');
+
 
 module.exports = { //exports data in Node.js so it can be require()d in other files
     data: new SlashCommandBuilder()
         .setName('emoji')
         .setDescription('manipulate and retrieve information about emojis')
-        .addSubcommand(display
+        .addSubcommand(displayemoji
             .addStringOption(option =>
                 option.setName('emoji')
                     .setDescription('the emoji to display')
                     .setRequired(true)
             )),
 
-
-
 //https://stackoverflow.com/questions/64053658/get-emojis-from-message-discord-js-v12
     async execute(interaction) {
         const cmd_name = interaction.options.getSubcommand();
         const EMOJIREGEX = /<a*:.+:(\d+)>/gm;
         switch (cmd_name) {
-            case 'display':
+            case 'display': //needs some error handling
                 const toDisplay = interaction.options.getString('emoji');
                 console.log(toDisplay);
                 const match = EMOJIREGEX.exec(toDisplay);
