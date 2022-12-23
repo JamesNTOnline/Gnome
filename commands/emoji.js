42caa0f9af5c093afacfbef8f3cd9313035206a9
jimjam2077 <james.nicholas.thomas@gmail.com>
2022-12-23 23:47:27 +0000

    export data

--- commands/emoji.js
+++ commands/emoji.js
@@ -14,24 +14,42 @@ function buildSubCommand(name, desc) { //this should go into a helper file
     return sub_cmd;
 }
 
-let displayemoji = buildSubCommand('display', 'displays a custom emoji at a bigger size');
-let addemoji = buildSubCommand('add', 'adds an emoji using an attachment');
-let delemoji = buildSubCommand('delete', 'deletes an emoji from the emoji list');
-let reactscore = buildSubCommand('score', 'shows the reaction score for a user');
+let display_emoji = buildSubCommand('display', 'displays a custom emoji at a bigger size');
+let add_emoji = buildSubCommand('add', 'adds an emoji using an attachment');
+let del_emoji = buildSubCommand('delete', 'deletes an emoji from the emoji list');
+let react_score = buildSubCommand('score', 'shows the reaction score for a user');
 
 
 module.exports = { //exports data in Node.js so it can be require()d in other files
     data: new SlashCommandBuilder()
         .setName('emoji')
         .setDescription('manipulate and retrieve information about emojis')
-        .addSubcommand(displayemoji
+        .addSubcommand(display_emoji
             .addStringOption(option =>
                 option.setName('emoji')
                     .setDescription('the emoji to display')
-                    .setRequired(true)
-            )),
+                    .setRequired(true)))
+        .addSubcommand(add_emoji
+            .addAttachmentOption(option =>
+                option.setName('attachment')
+                    .setDescription('image attachment to add as an emoji')
+                    .setRequired(true))
+            .addStringOption(option =>
+                option.setName('name')
+                    .setDescription('tag for the new emoji')
+                    .setRequired(true)))
+        .addSubcommand(del_emoji
+            .addStringOption(option =>
+                option.setName('emoji')
+                    .setDescription('the emoji to be deleted from this server')
+                    .setRequired(true)))
+        .addSubcommand(react_score
+            .addUserOption(option =>
+                option.setName('user')
+                    .setDescription('who to check the reaction score for')
+                    .setRequired(true))),
 
-//https://stackoverflow.com/questions/64053658/get-emojis-from-message-discord-js-v12
+    //https://stackoverflow.com/questions/64053658/get-emojis-from-message-discord-js-v12
     async execute(interaction) {
         const cmd_name = interaction.options.getSubcommand();
         const EMOJIREGEX = /<a*:.+:(\d+)>/gm;
