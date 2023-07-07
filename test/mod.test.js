25bf7bcd0e3db92f5a3bc1a467af9e688a0a5429
jimjam2077 <james.nicholas.thomas@gmail.com>
2023-07-07 22:04:21 +0100

    refactor + negative tests

--- test/mod.test.js
+++ test/mod.test.js
@@ -13,36 +13,17 @@ const mockGuild = {
         kick: jest.fn().mockResolvedValue(), // Add the kick method mock
         cache: {
             get: jest.fn().mockReturnValue(true),
-
-        },
-    },
+        }
+    }
 };
 
 const mockAdmin = {
     id: '284930711566155777',
     permissions: {
         has: jest.fn().mockReturnValue(true),
-    },  // An instance of Permissions with the KICK_MEMBERS flag
+    }
 };
 
-
-function mockInteraction(options) {
-    return {
-        id: '123456789',
-        guildId: '580797956983226379',
-        channelId: '1048732929473384538',
-        guild: mockGuild,
-        member: mockAdmin,
-        client: options.client,
-        options: {
-            getSubcommand: jest.fn().mockReturnValue(options.subcommand),
-            getUser: jest.fn().mockReturnValue(targetMock),
-            getString: jest.fn().mockReturnValue(options.reason),
-            getInteger: jest.fn().mockReturnValue(options.delete),
-        },
-        reply: jest.fn(),
-    };
-}
 
 describe('/mod commands', () => {
     let client;
@@ -75,38 +56,91 @@ describe('/mod commands', () => {
 
 
     test('testing a successful /mod kick command', async () => {
-        const kickMock = jest.fn().mockResolvedValue(); // Mock the kick method
-        const interaction = mockInteraction({
-            client: client,
-            subcommand: 'kick',
-            reason: 'Reason for kick',
-            delete: 0,
-        });
-        // Mock the kick method within the guild members cache
+        const kickMock = jest.fn().mockResolvedValue();
         mockGuild.members.kick = kickMock;
+
+        const interaction = setupInteraction(client, 'kick', 'Reason for kick', 0);
         await moderation.execute(interaction);
-        // Assert that the reply function was called with the expected embed - in this case i simply expect an embed, 
-        // but could specify which fields are included.
-        expect(interaction.reply).toHaveBeenCalledWith({ embeds: [expect.any(Object)] });
-        expect(kickMock).toHaveBeenCalledWith(targetMock, 'Reason for kick'); //check that the user was kicked with the reason specified earlier
+
+        assertSuccessfulCommandExecution(interaction, kickMock, [targetMock, 'Reason for kick']);
     });
 
+    test('testing a failed /mod kick command due to insufficient permissions', async () => {
+        const kickMock = jest.fn().mockResolvedValue();
+        mockAdmin.permissions.has.mockImplementation((flag) => {
+            return flag === PermissionsBitField.Flags.KickMembers ? false : true;
+        });
+        mockGuild.members.kick = kickMock;
+
+        const interaction = setupInteraction(client, 'kick', 'Reason for kick', 0);
+        await moderation.execute(interaction);
+
+        assertInsufficientPermissions(interaction, kickMock);
+    });
 
     test('testing a successful /mod ban command', async () => {
-        const banMock = jest.fn().mockResolvedValue(); // Mock the kick method
-        const interaction = mockInteraction({
-            client: client,
-            subcommand: 'ban',
-            reason: 'Reason for ban',
-            delete: 86400,
-        });
-
-        // Mock the kick method within the guild members cache
+        const banMock = jest.fn().mockResolvedValue();
         mockGuild.members.ban = banMock;
+
+        const interaction = setupInteraction(client, 'ban', 'Reason for ban', 86400);
         await moderation.execute(interaction);
-        // Assert that the reply function was called with the expected embed - in this case i simply expect an embed, 
-        // but could specify which fields are included.
-        expect(interaction.reply).toHaveBeenCalledWith({ embeds: [expect.any(Object)] });
-        expect(banMock).toHaveBeenCalledWith(targetMock, {deleteMessageSeconds: 86400, reason: "Reason for ban"}); //check that the user was kicked with the reason specified earlier
+
+        assertSuccessfulCommandExecution(interaction, banMock, [targetMock, { deleteMessageSeconds: 86400, reason: 'Reason for ban' }]);
+    });
+
+    test('testing a failed /mod ban command due to insufficient permissions', async () => {
+        const banMock = jest.fn().mockResolvedValue();
+        mockAdmin.permissions.has.mockImplementation((flag) => {
+            return flag === PermissionsBitField.Flags.BanMembers ? false : true;
+        });
+        mockGuild.members.ban = banMock;
+
+        const interaction = setupInteraction(client, 'ban', 'Reason for ban', 0);
+        await moderation.execute(interaction);
+
+        assertInsufficientPermissions(interaction, banMock);
     });
 });
+
+
+
+function mockInteraction(options) {
+    return {
+        id: '123456789',
+        guildId: '580797956983226379',
+        channelId: '1048732929473384538',
+        guild: mockGuild,
+        member: mockAdmin,
+        client: options.client,
+        options: {
+            getSubcommand: jest.fn().mockReturnValue(options.subcommand),
+            getUser: jest.fn().mockReturnValue(targetMock),
+            getString: jest.fn().mockReturnValue(options.reason),
+            getInteger: jest.fn().mockReturnValue(options.delete),
+        },
+        reply: jest.fn(),
+    };
+}
+
+// Helper function to setup the interaction object
+function setupInteraction(client, subcommand, reason, deleteValue) {
+    const interaction = mockInteraction({
+        client: client,
+        subcommand: subcommand,
+        reason: reason,
+        delete: deleteValue,
+    });
+    return interaction;
+}
+
+// Helper function to assert the successful execution of a command
+function assertSuccessfulCommandExecution(interaction, mockMethod, expectedOptions) {
+    expect(interaction.reply).toHaveBeenCalledWith({ embeds: [expect.any(Object)] });
+    expect(mockMethod).toHaveBeenCalledWith(...expectedOptions);
+}
+
+// Helper function to assert the failure of a command due to insufficient permissions
+function assertInsufficientPermissions(interaction, mockMethod) {
+    expect(interaction.reply).toHaveBeenCalledWith('You don\'t have permission for that!');
+    expect(mockMethod).not.toHaveBeenCalled();
+}
