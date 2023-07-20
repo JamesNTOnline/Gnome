d9506529291d5a8ee6d69f211f57511d7e7f4f4a
jimjam2077 <james.nicholas.thomas@gmail.com>
2023-07-20 10:25:00 +0100

    tidying up files

--- commands/info.js
+++ commands/info.js
@@ -16,3 +16,32 @@ module.exports = { //exports data in Node.js so it can be require()d in other fi
         await interaction.reply('ph');
     },
 };
+
+
+
+
+// const { SlashCommandBuilder } = require('discord.js');
+
+// //https://discordjs.guide/slash-commands/response-methods.html#ephemeral-responses
+
+// module.exports = { //exports data in Node.js so it can be require()d in other files
+//     data: new SlashCommandBuilder()
+//         .setName('ping')
+//         .setDescription('Replies to let you know the bot is active'),
+//     async execute(interaction){
+//         await interaction.reply('I am ready to work!');
+//     },
+// };
+
+// const { SlashCommandBuilder } = require('discord.js');
+
+// //https://discordjs.guide/slash-commands/response-methods.html#ephemeral-responses
+
+// module.exports = { //exports data in Node.js so it can be require()d in other files
+//     data: new SlashCommandBuilder()
+//         .setName('echo')
+//         .setDescription('Repeats a phrase back to you'),
+//     async execute(interaction){
+//         await interaction.reply('Placeholder');
+//     },
+// };
