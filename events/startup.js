3c51c98ece9a55f1f77a76a95e4945c473ac2597
jimjam2077 <james.nicholas.thomas@gmail.com>
2023-07-21 07:28:11 +0100

    tidying up event

--- events/startup.js
+++ events/startup.js
@@ -7,12 +7,3 @@ module.exports = {
         console.log(`Startup successful! Logged in as ${client.user.tag}`);
     }
 };
-
-//DEPRECATED CODE
-// when client ready, run this once
-// 'c' s event parameter to keep it separate from the 'client'
-//client.once(Events.ClientReady, c =>{
-//    console.log(`Ready! Logged in as ${c.user.tag}`);
-//    c.user.setPresence({ activities: [{ name: 'Searching for Gnomefriends' }],
-//    status: 'idle' });
-//});
