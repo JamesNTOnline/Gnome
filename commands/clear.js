19cf313c2e3d5135b58ee4a08d95543910d0917e
jimjam2077 <james.nicholas.thomas@gmail.com>
2023-07-22 09:11:05 +0100

    mass unban command working fine

--- commands/clear.js
+++ commands/clear.js
@@ -5,9 +5,10 @@
  * @todo - command to remove a role from users
  * @todo - command to purge messages
  * @todo - command to remove emojis from messages
- * 
+ * @todo - a more robust permission check
+ * @todo - don't try to alter non-moderatable members
 */
-const { SlashCommandBuilder, SlashCommandSubcommandBuilder, PermissionsBitField} = require('discord.js');
+const { SlashCommandBuilder, SlashCommandSubcommandBuilder, PermissionsBitField } = require('discord.js');
 const SubOptionBuilder = require('../builders/sub-option-builder');
 
 
@@ -24,7 +25,7 @@ let messages = new SubOptionBuilder('messages').getSubCmd()
             .setRequired(false));
 
 
-module.exports = { 
+module.exports = {
     data: new SlashCommandBuilder()
         .setName('clear')
         .setDescription('[PH]')
@@ -45,126 +46,46 @@ module.exports = {
                  */
                 case 'timeouts':
                     //anon filter function
-                    const timeoutFilter = (member) => member.communicationDisabledUntilTimestamp > 0;
+                    const timeoutFilter = (member) => member.communicationDisabledUntilTimestamp > 0 && member.manageable;
                     processMembers(cmdName, interaction, timeoutFilter, async (member) => {
                         // Discord API function to apply a null timeout (removes timeout)
                         await member.timeout(null);
                     });
                     break;
-                // interaction.guild.members.fetch()
-                //     .then(async members => {
-                //         // Filter to members that have communication disabled
-                //         const disabledMembers = members.filter(member => member.communicationDisabledUntilTimestamp > 0);
-                //         const totalCount = disabledMembers.size;
-
-                //         await interaction.reply(`Clearing ${cmdName} from 0/${totalCount} members...`);
-
-                //         let count = 0;
-                //         for (const member of disabledMembers.values()) {
-                //             try {
-                //                 await member.timeout(null);
-                //                 count++;
-                //                 await interaction.editReply(`Cleared ${cmdName} from ${count}/${totalCount} members...`);
-                //             } catch (err) {
-                //                 // Handle any errors that occur during the timeout process for a member
-                //                 console.error(`Error clearing ${cmdName.slice(0, -1)} for member ${member.user.tag}:`, err);
-                //             }
-                //         }
-
-                //         await interaction.followUp(`${cmdName.charAt(0).toUpperCase()} cleared!`);
-                //     })
-                //     .catch(err => {
-                //         interaction.deleteReply();
-                //         interaction.followUp('Something went wrong, check audit log');
-                //         console.error(err);
-                //     });
-                // break;
-
-                // try {
-                //     let members = await interaction.guild.members.fetch();
-                //     let count = 0;
-                //     // Filter to members that have communication disabled
-                //     members = members.filter(member => member.communicationDisabledUntilTimestamp > 0);
-                //     await interaction.reply(`Clearing timeouts from ${count}/${members.size} members...`);
-                //     for (const member of members.values()) {
-                //         // Check permissions else remove timeout
-                //         try {
-                //             await member.timeout(null);
-                //             count++;
-                //             await interaction.editReply(`Cleared timeouts from ${count}/${members.size} members...`);
-                //         } catch (err) {
-                //             // Handle any errors that occur during the timeout process for a member
-                //             console.error(`Error clearing timeout for member ${member.user.tag}:`, err);
-                //         }
-                //     }
-                //     await interaction.followUp('Timeouts cleared!');
-                // } catch (err) {
-                //     await interaction.deleteReply();
-                //     await interaction.followUp('Something went wrong, check audit log');
-                //     console.error(err);
-                // }
-                // break;
                 case 'nicknames':
                     //anon filter function
-                    const nickFilter = (member) => member.nickname !== null;
+                    const nickFilter = (member) => member.nickname !== null && member.manageable;
                     processMembers(cmdName, interaction, nickFilter, async (member) => {
-                        // Discord API function to apply a null timeout (removes timeout)
+                        // null removes nickname
                         await member.setNickname(null);
                     });
                     break;
-                //     interaction.guild.members.fetch()
-                //     .then(async members => {
-                //         // Filter to members that have communication disabled
-                //         const namedMembers = members.filter(member => member.nickname !== null);
-                //         const totalCount = namedMembers.size;
-
-                //         await interaction.reply(`Clearing ${cmdName} from 0/${totalCount} members...`);
-
-                //         let count = 0;
-                //         for (const member of namedMembers.values()) {
-                //             try {
-                //                 await member.setNickname(null);
-                //                 count++;
-                //                 await interaction.editReply(`Cleared ${cmdName} from ${count}/${totalCount} members...`);
-                //             } catch (err) {
-                //                 // Handle any errors that occur during the timeout process for a member
-                //                 console.error(`Error clearing ${cmdName.slice(0, -1)} for member ${member.user.tag}:`, err);
-                //             }
-                //         }
-
-                //         await interaction.followUp(`${cmdName.charAt(0).toUpperCase()} cleared!`);
-                //     })
-                //     .catch(err => {
-                //         interaction.deleteReply();
-                //         interaction.followUp('Something went wrong, check audit log');
-                //         console.error(err);
-                //     });
-                // break;
                 case 'bans':
-                    const blueSquareEmoji = ':blue_square:';
-                    await interaction.reply('Starting to clear the ban list\n[                              ]')
-                        .then(async () => { //allows using await to do sequential async functions
+                    await interaction.reply('Starting to clear the ban list...')
+                        .then(async () => {
                             const bans = await interaction.guild.bans.fetch();
                             const banCount = bans.size;
                             if (banCount === 0) {
-                                return interaction.editReply('No members are banned in this server.');
+                                await interaction.editReply('No members are banned in this server.');
+                                return;
                             }
-                            let counter = 0;
+                            let count = 0;
                             for (const ban of bans.values()) {
-                                await interaction.guild.members.unban(ban.user);
-                                counter++;
-                                const interval = Math.floor(bans.size / 6); // Calculate the interval for replacing spaces with a blue square
-                                if (interval > 0 && counter % interval === 0) {
-                                    const blueSquareCount = Math.floor(counter / interval);
-                                    const spaceCount = 6 - (blueSquareCount % 6);
-                                    const modifiedMessage = `[${blueSquareEmoji.repeat(blueSquareCount)}${'      '.repeat(spaceCount)}]`;
-                                    await interaction.editReply(modifiedMessage);
-                                }
+                                await interaction.guild.members.unban(ban.user)
+                                    .then(async () => {
+                                        count++;
+                                        await interaction.editReply(`Unbanned ${ban.user}: ${count}/${banCount} completed`);
+                                    })
+                                    .catch((err) => {
+                                        console.error('Error while unbanning:', err);
+                                    });
                             }
+                            await interaction.followUp(`Finished!`);
                         })
                         .catch(err => {
                             console.error(err);
-                            interaction.editReply('An error occurred while processing the command.');
+                            interaction.deleteReply();
+                            interaction.followUp('Something went wrong, check audit log');
                         });
                     break;
                 case 'reactions':
@@ -181,30 +102,37 @@ module.exports = {
 };
 
 
+/**
+ * 
+ * @param {string} cmdName - The name of the command
+ * @param {Interaction} interaction - The interaction object from discord.js
+ * @param {Function} filterCondition - A function which filters the guildmember list
+ * @param {Function} functionToCall - The function to be called for each filtered (included) member
+ */
 async function processMembers(cmdName, interaction, filterCondition, functionToCall) {
     try {
-      const members = await interaction.guild.members.fetch();
-      // Filter members based on the provided filter condition
-      const targetMembers = members.filter(filterCondition);
-  
-      const totalCount = targetMembers.size;
-      await interaction.reply(`Processing ${cmdName} for 0/${totalCount} members...`);
-  
-      let count = 0;
-      for (const member of targetMembers.values()) {
-        try {
-          await functionToCall(member);
-          count++;
-          await interaction.editReply(`Processed ${cmdName} for ${count}/${totalCount} members...`);
-        } catch (err) {
-          console.error(`Error during processing ${cmdName} for member ${member.user.tag}:`, err);
+        const members = await interaction.guild.members.fetch();
+        const targetMembers = members.filter(filterCondition);
+
+        const totalCount = targetMembers.size;
+        await interaction.reply(`Processing ${cmdName} for 0/${totalCount} members...`);
+
+        let count = 0;
+        for (const member of targetMembers.values()) {
+            try {
+                // process members with a member function
+                await functionToCall(member);
+                count++;
+                await interaction.editReply(`Processed ${cmdName} for ${count}/${totalCount} members...`);
+            } catch (err) {
+                console.error(`Error during processing ${cmdName} for member ${member.user.tag}:`, err);
+            }
         }
-      }
-  
-      await interaction.followUp(`${cmdName.charAt(0).toUpperCase() + cmdName.slice(1)} cleared!`);
+
+        await interaction.followUp(`${cmdName.charAt(0).toUpperCase() + cmdName.slice(1)} cleared!`);
     } catch (err) {
-      interaction.deleteReply();
-      interaction.followUp('Something went wrong, check audit log');
-      console.error(err);
+        interaction.deleteReply();
+        interaction.followUp('Something went wrong, check audit log');
+        console.error(err);
     }
-  }
+}
